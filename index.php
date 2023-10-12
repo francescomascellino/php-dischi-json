448dea1e81e8df02adccb162da2167c1499b6fdd
francescomascellino <franesco.mascellino@gmail.com>
2023-10-12 15:42:23 +0200

    link modal to cards

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 
     <div id="app">
 
+        <!-- NAV -->
         <nav class="navbar navbar-expand-sm navbar-light bg-secondary-subtle">
             <div class="container">
                 <div class="row">
@@ -34,9 +35,28 @@
 
             <div class="row my-3 g-3 p-2">
 
+
+                <!-- MODAL -->
+                <div class="modal fade" :id="`${disk.author.replace(/\s+/g, '')}${index}`" tabindex="-1" :aria-labelledby="`${disk.author.replace(/\s+/g, '')}${index}Label`" aria-hidden="true" v-for="(disk, index) in disks" :key="`${disk.author.replace(/\s+/g, '')}${index}`">
+
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" :id="`${disk.author.replace(/\s+/g, '')}${index}`">{{disk.title}}</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body text-center">
+                                <img :src="disk.poster" :alt="disk.title" style="width: 100%;" class="mb-2">
+                                <h3>{{disk.author}}</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
                 <!-- DISK CARDS -->
-                <div class="col-4" v-for="(disk, index) in disks" :key="index">
-                    <div class="card bg-dark-subtle">
+                <div class="col-4" v-for="(disk, index) in disks" :key="`${disk.author.replace(/\s+/g, '')}${index}`">
+                    <div class="card bg-dark-subtle" data-bs-toggle="modal" :data-bs-target="`#${disk.author.replace(/\s+/g, '')}${index}`" :id="`${disk.author.replace(/\s+/g, '')}${index}`">
                         <img :src="disk.poster" class="card-img-top" :alt="disk.title">
                         <div class="card-body text-center">
                             <h5 class="card-title">{{disk.title}}</h5>
@@ -59,6 +79,9 @@
 
 <!-- AXIOS CDN -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.1/axios.min.js" integrity="sha512-emSwuKiMyYedRwflbZB2ghzX8Cw8fmNVgZ6yQNNXXagFzFOaQmbvQ1vmDkddHjm5AITcBIZfC7k4ShQSjgPAmQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- BS CDN -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 <!-- JS -->
 <script src='./main.js'></script>
